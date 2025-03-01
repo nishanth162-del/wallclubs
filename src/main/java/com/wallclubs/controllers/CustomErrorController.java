456391d6ff3466744747213c5d1ff693a2f9e7b0
Nishanth Reddy A S <113271880+nishanth162-del@users.noreply.github.com>
2025-03-01 16:51:03 +0530

    Ads added

---
+++ src/main/java/com/wallclubs/controllers/CustomErrorController.java
@@ -0,0 +1,15 @@
+package com.wallclubs.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        // Return custom 404 page—can expand for other errors later
+        return "404";
+    }
+}
